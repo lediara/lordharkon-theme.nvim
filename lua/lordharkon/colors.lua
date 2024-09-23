0e3c65618eff1dca9273d7f3147eefd0de0a1d06
Peter Kilian <kilianpeter119@gmail.com>
2024-09-23 12:16:03 +0200

    trying to get something that works

--- lua/lordharkon/colors.lua
+++ lua/lordharkon/colors.lua
@@ -1,3 +1,23 @@
 local M = {}
 
-M.default {}
+M.default = {}
+
+---@return ColorScheme
+function M.setup(opts)
+    opts = opts or {}
+    local config = require("lordharkon.config")
+
+    -- local style = config.is_day() and config.options.light_style or config.options.style
+    local style = "default"
+    local palette = M[style] or {}
+    if type(palette) == "function" then
+        palette = palette()
+    end
+
+    -- Color Palette
+    ---@class ColorScheme: Palette
+    local colors = vim.tbl_deep_extend("force", vim.deepcopy(M.default), palette)
+    return colors
+end
+
+return M
